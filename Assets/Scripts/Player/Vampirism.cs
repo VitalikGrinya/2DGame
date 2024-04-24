d631c1fc4a6db84d17cfd797b94c8fa974699758
VitalikGrinya <vitalik240899@ya.ru>
2024-04-24 11:48:27 +0300

    Vampirism.cs

--- Assets/Scripts/Player/Vampirism.cs
+++ Assets/Scripts/Player/Vampirism.cs
@@ -6,7 +6,8 @@ public class Vampirism : MonoBehaviour
     [SerializeField] private float _radius = 5f;
     [SerializeField] private LayerMask _layerEnemy;
     [SerializeField] private HealthValueChanger _playerHealth;
-    [SerializeField] private EnemyHealth _enemyHealth;
+    [SerializeField] private EnemyHealth[] _enemyHealth;
+    [SerializeField] private GameObject _vampireRing;
 
     private int _damage = 10;
     private int _heal;
@@ -18,7 +19,9 @@ public class Vampirism : MonoBehaviour
     public void UseAbility()
     {
         if (_coroutine != null)
+        {
             StopCoroutine(_coroutine);
+        }  
 
         _coroutine = StartCoroutine(StealHealth());
     }
@@ -26,20 +29,26 @@ public class Vampirism : MonoBehaviour
     private IEnumerator StealHealth()
     {
         _heal = _damage / 2;
+
         WaitForSeconds delay = new WaitForSeconds(_timeCoroutine);
 
         _collider = Physics2D.OverlapCircle(transform.position, _radius, _layerEnemy);
 
-        if (_collider == null)
+        if(_collider)
         {
+            _vampireRing.SetActive(true);
+        }
+        else
+        {
+            _vampireRing.SetActive(false);
             yield break;
         }
 
-        for (int i = 0; i < _timeSteal * Time.deltaTime; i++)
+        for (int i = 0; i < _timeSteal; i++)
         {
             if (_collider.TryGetComponent(out HealthChanger enemyHealth) == true)
             {
-                if (_enemyHealth.CurrentHealth <= _damage)
+                if (_enemyHealth[0].CurrentHealth <= _damage || _enemyHealth[1].CurrentHealth <= _damage)
                 {
                     _damage = 0;
                 }
